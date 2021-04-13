e119f712c84bcba53376c2cd66efafedcf8a2966
VladyslavGennadijovych <82404622+VladyslavGennadijovych@users.noreply.github.com>
2021-04-13 19:12:26 +0300

    Add files via upload

---
+++ HW1.1.rb
@@ -0,0 +1,54 @@
+#1
+ #Integer
+  a=1
+  b=333
+   print a , "\n"
+   p a+b
+   puts b-a
+  #String
+  s="I love football"
+   puts s
+   print s , "\n"
+   p s
+ #Boolean
+  k=true
+  p=false
+   p k
+   print p , "\n"
+   puts p
+ #Symbol
+  symbol = :a
+   p symbol
+   print symbol , "\n"
+   puts symbol
+ #Array
+  ar = [1,2,3,4,5,6]
+   p ar
+   print ar[3] , "\n"
+   puts ar
+ #Hash
+  hash={:x=>12,:y=>13,:z=>14}
+   p hash
+   puts hash
+   print hash ,"\n"
+
+#2
+  apple=130
+  prune=170
+   fruits=apple+prune
+   print ("The sum of apples and prunes = "),fruits ,"\n"
+
+#3
+ hash_1 = Hash.new
+  puts hash_1
+ hash_2 = Hash[]
+  print hash_2 ,"\n"
+ hash_3 = {}
+  p hash_3
+
+ array_1=Array[]
+  print array_1 ,"\n"
+ array_2=[]
+  p array_2
+ array_3=Array.new
+  p array_3
