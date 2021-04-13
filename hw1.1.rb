382b28a81621ab10afe24b6effed3d8161a1a322
Ivan <ivankozmulyak@gmail.com>
2021-04-13 13:17:13 +0300

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,46 @@
+# ex1
+integer1 = 1
+boolean1 = false
+string1 = 'Mykola Ilashchuk is the best'
+symbol1 = 's'
+array1 = Array[5, 't', "Ivan"]
+hash1 = { 'a': 1, 'b': 2, 'c': 321 }
+p integer1
+p boolean1
+p string1
+p symbol1
+p array1
+p hash1
+puts integer1
+puts boolean1
+puts string1
+puts symbol1
+puts array1
+puts hash1
+print integer1
+print boolean1
+print string1
+print array1
+print hash1
+
+# ex2
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+# ex3
+hash2 = Hash.new
+hash3 = Hash[]
+hash4 = {}
+puts hash2
+puts hash3
+puts hash4
+array2 = Array.new
+array3 = []
+array4 = Array[]
+array5 = {}
+puts array2
+puts array3
+puts array4
+puts array5
