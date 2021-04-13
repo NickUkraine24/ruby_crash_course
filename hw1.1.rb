0e3476338e7565bc0265aa559929e4d605c51675
Pi2l <nightmisha8@gmail.com>
2021-04-13 14:54:57 +0300

    complete first task

---
+++ hw1.1.rb
@@ -0,0 +1,42 @@
+# 1
+int = 1
+bool = true
+string = 'ttt'
+sym = :fff
+arr = [1,23,4,5,3,4,4]
+hash = { a: '10', b: '11', c:"12"}
+
+print int
+print bool
+print string
+print sym
+print arr
+print hash
+
+puts int
+puts bool
+puts string
+puts sym
+puts arr
+puts hash
+
+p int
+p bool
+p string
+p sym
+p arr
+p hash
+
+# 2
+apple = 130
+prune = 170
+puts summ = apple + prune
+
+# 3
+a1 = Hash[]
+a2 = Hash.new
+a3 = {}
+
+b1 = []
+b2 = Array.new
+b3 = Array[]
