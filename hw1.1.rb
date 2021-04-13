cf038a046d9a3ad2b062644d247c311e6075aa6b
Kolyanuss <mykola.1zt@gmail.com>
2021-04-13 14:46:43 +0300

    1. Create variables by all types of Ruby(Integer, Boolean, String, Symbol, Array, and Hash) and output these variables by p, puts, print.

--- hw1.1.rb
+++ hw1.1.rb
@@ -1,1 +1,40 @@
-push "Hola"
+print "Hello Ruby Crash Course"
+
+bol = true
+integ = 1
+dubl = 1.2
+str = 'str'
+syb = :lol
+arr1 = [1,2,4,8,16,32,64,128]
+hessh = {a: 'hello', b: 'world', c:'!'}
+
+
+p bol
+puts integ
+print dubl
+print str+"\n"
+puts syb
+puts arr1.inspect
+print hessh
+print "\nkey: " + String.new(hessh.keys.inspect)
+print "\nvalues: " + hessh.values.inspect
+
+print "\nFruits:\n"
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+
+empty1 = Hash[]
+empty2 = Hash.new
+puts empty1
+puts empty2
+
+empty3 = []
+empty4 = Array.new
+empty5 = Array[]
+puts empty3
+puts empty4
+puts empty5
+
