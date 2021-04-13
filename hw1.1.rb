ac72817b566cb56989e73acca22d8b004cbb19e5
ZiM1231 <mykolyshyn.z31@gmail.com>
2021-04-13 22:11:00 +0300

    first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,43 @@
+# ex1
+#Integer
+x = 5
+y = 7
+p x
+puts y
+print 36
+
+#String 
+p "Hello"
+puts 'Mykolyshyn Pavlo'
+print "String"
+
+#Symbol 
+:ruby_rules 
+
+#Array 
+ary = [1, 2, "one", 2.5]
+p ary
+puts ary
+print ary
+
+#Hash
+h = {foo: 0, bar: 1, baz: 2}
+
+# ex2
+apple = 130
+prune = 170
+fruits = apple + prune
+print fruits
+
+# ex3
+#Array
+ary = Array.new
+Array.new(5)
+Array.new(4, true)
+arr = [ , , ]
+
+#Hash
+person = {name: 'Matz', language: 'Ruby'}
+h = {foo: 0, bar: 1, baz: 2}
+
+
