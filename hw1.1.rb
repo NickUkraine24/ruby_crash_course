711ac492df04e789b85c9559f5b10b595d99ee75
Olha-30 <vasianovych.olha@chnu.edu.ua>
2021-04-13 22:22:44 +0300

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,47 @@
+ #task 1
+ digits = 30
+ number = 30.03
+ smile = TRUE
+ words = 'Go for it'
+ my_symb = :nice
+ arr = [ "stars", "sky", 3.14, 56, "sun" ]
+ rainbow = { red: 1, orange: 2, yellow: 3, green: 4, skyblue: 5, blue: 6, purple: 7 }
+
+
+ def output(data) 
+  p data
+  puts data
+  print "#{data}\n"
+
+ end
+
+ output digits 
+ output number
+ output smile
+ output words 
+ output my_symb 
+ output arr
+ output rainbow
+
+#task 2
+ apple = 130
+ prune = 170
+ fruits = apple + prune
+
+ p fruits 
+
+ #task 3
+ hash_1 = Hash.new
+ hash_2 = Hash[]
+ hash_3 = {}
+
+ array_1 = []
+ array_2 = Array.new
+ array_3 = Array.new(5) {Array.new(5)}
+
+
+
+
+
+
+
