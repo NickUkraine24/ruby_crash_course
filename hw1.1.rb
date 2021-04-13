8bbe05d21e9c352416e909575a29262e3c78129c
Александра <oleksandratisenko59@gmail.com>
2021-04-13 18:03:31 +0300

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,55 @@
+#task 1
+
+int=1
+p int
+print (int) #без переносу
+puts int
+
+bool=true
+p bool
+print bool #без переносу
+puts bool
+
+Str="hello world!"
+p Str # з лапками
+print Str # без лапок і переносу
+puts Str # без лапок
+
+arr=[23,45,9]
+p arr
+print arr # без переносу
+puts  arr #без квадратних дужок і переніс після кожного елементу масиву
+
+
+h = {:foo => 1, :bar => 2, :baz => 3}
+p h
+print h # без переносу
+puts h
+
+Sym=:a
+p Sym
+print Sym # без переносу
+puts Sym
+
+
+#task 2
+apple=130
+prune=170
+fruits=apple+prune
+p fruits
+
+#task 3
+h1 = Hash.new
+p h1
+h2 = Hash[]
+p h2
+h3 = {}
+p h3
+
+arr1=[]
+p arr1
+arr2=Array.new
+p arr2
+arr3=Array[]
+p arr3
+
