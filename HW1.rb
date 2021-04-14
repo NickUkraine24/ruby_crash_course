1c30dcdcaf18c5ff6a94f41d7ff4e053a0b7c5b6
MaksMaryanchuk <lion20914king@gmail.com>
2021-04-14 13:00:07 +0300

    firstcommit

---
+++ HW1.rb
@@ -0,0 +1,52 @@
+#1
+#integer
+int = 1
+print (int)
+print("\n")
+#string
+string = "Hello Nick! "
+str2 = "How are you?"
+
+print string
+puts str2
+#bool
+
+bool = true 
+p bool
+puts "-------------"
+puts bool
+
+#array
+
+mass = [12,32,41,1]
+
+p mass
+puts "-----------"
+print mass
+print("\n")
+#Hash
+h = {:foo=> 2, :bar=>5, :bash=>4}
+p h
+puts "-------------"
+puts h
+
+#2
+
+apple = 130 
+prune =170
+
+fruits = apple + prune
+
+puts fruits
+
+#3 
+hash1 = Hash[]
+p hash1
+hash2 = Hash.new
+p hash2
+
+mass1 = []
+p mass1
+
+mass2 = Array.new
+p mass2
