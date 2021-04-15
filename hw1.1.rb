67bac106a3b9ac93e889d2b479eae95a3e8946ba
Sasha-Kuba <sasakuba54@gmail.com>
2021-04-15 03:27:22 +0300

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,32 @@
+#1
+int = 15
+bolean = true
+string = "a string"
+symbol = 'some symbols'
+arr = [1,2,3]
+h = { "a" => 4, "c" => 5 }
+
+p int 
+puts bolean
+puts string
+puts symbol
+p arr
+puts h["a"] , h["c"]
+
+#2
+
+apple = 130
+prune = 170
+fruit = apple + prune
+print fruit
+
+#3
+h = {}
+
+h = Hash[]
+
+h = Hash.new
+
+array = []
+
+arry = Array.new
