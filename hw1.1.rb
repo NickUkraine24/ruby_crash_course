17e64ded9a8cb5517b9caf38208fb8d85ccbeac8
Hrynkevych Vitaliy <apec1325@gmail.com>
2021-04-16 15:37:32 +0300

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,54 @@
+#1
+ #Integer
+  a=13
+  b=25
+   print a , "\n"
+   p a+b
+   puts b-a
+  #String
+  с="Ruby crash courses"
+   puts c
+   print c , "\n"
+   p c
+ #Boolean
+  d=true
+  e=false
+   p d
+   print e , "\n"
+   puts d
+ #Symbol
+  symbol = :f
+   p symbol
+   print symbol , "\n"
+   puts symbol
+ #Array
+  mas = [1,2,3,4,5,6]
+   p mas
+   print mas[3] , "\n"
+   puts mas
+ #Hash
+  hash={:x=>13,:y=>25,:z=>78}
+   p hash
+   puts hash
+   print hash ,"\n"
+
+#2
+  apple=130
+  prune=170
+   fruits=apple+prune
+   print ("The sum of apples and prunes = "),fruits ,"\n"
+
+#3
+ hash_1 = Hash.new
+  puts hash_1
+ hash_2 = Hash[]
+  print hash_2 ,"\n"
+ hash_3 = {}
+  p hash_3
+
+ mas_1=Array[]
+  print mas_1 ,"\n"
+ mas_2=[]
+  p mas_2
+ mas_3=Array.new
+  p mas_3
