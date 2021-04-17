81df6ac5d505dcd11b017d32fa4d3b76a7b5f856
0xKolyanus <mukola.1zt.@gmail.com>
2021-04-17 21:26:11 +0300

    complete hw1.2.rb

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,2 +1,10 @@
 food = %W(bacon orange apple)
+p food.dig(0)
+p food[-1]
+food.push("yogurt")
 p food
+hash_1 = Hash[UA: "Ukraine", JP: "Japan", WK: "Wakanda"]
+puts hash_1
+course = "course"
+p course.gsub!("ourse", "anada")
+p course.upcase
