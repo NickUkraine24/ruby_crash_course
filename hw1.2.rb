5b42ed2ee35b934a69cb578589df7e464307440d
khannah13 <hannakhomko@gmail.com>
2021-04-17 20:01:11 +0300

    added first commit for hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,15 @@
+#ex1
+food = ["bacon", "orange", "apple"]
+p food [0]
+p food [-1]
+food.push ("yogurt")
+p food
+
+#ex2
+country = {Ukraine: "UA", Turkey: "TR", Togo: "TG"}
+p country 
+
+#ex3
+string = "course"
+p string.tr('ourse', 'anada').upcase
+
