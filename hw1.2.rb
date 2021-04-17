adfc5b2c2eabbb2ec56e1a82edd5f57fd2be9c21
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-17 14:07:07 +0300

    finished hw 2

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,12 +1,25 @@
+#Ex 2
 food = %W(bacon orange apple)
-
 p food
 
+#Ex 3
 puts "First element: #{food.first}"
 
+#Ex 4
 puts "Last element: #{food.last}"
 
-food << "yogurt"
+#Ex 5
+food << 'yogurt'
 
 p food
 
+#Ex 6
+contries = { it: 'Italy', de: 'Germany', ro: 'Romania' }
+
+#Ex 7
+str = 'course'
+# 1: 
+puts "\"#{str}\" replaced with \"#{str.gsub("ourse", "anada")}\"" 
+
+# 2: 
+puts "Uppercase string : \"#{str.upcase}\"" 
