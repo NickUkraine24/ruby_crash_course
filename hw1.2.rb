75118b393f255d14d7a576a0185daf7ea4b1938b
Pi2l <nightmisha8@gmail.com>
2021-04-17 21:27:09 +0300

    complete w/h2

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,1 +1,17 @@
-p "tets1"
+food = ['bacon', 'orange', 'apple']
+p food 
+
+p food[0]
+p food[-1]
+p food.insert(1, 'yogurt')
+
+countries = { 
+    ES: 'Spain', 
+    US: 'United States', 
+    UK: 'United Kingdom' 
+}
+
+p countries
+
+course = 'course'
+p course.gsub('ourse', 'anada').upcase
