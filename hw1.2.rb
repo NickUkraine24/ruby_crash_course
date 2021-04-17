d5267259ce9774a03ef650e57e96d62bc8fb348d
NikolaySkrip <82404274+NikolaySkrip@users.noreply.github.com>
2021-04-17 22:25:01 +0300

    Add files via upload

---
+++ hw1.2.rb
@@ -0,0 +1,11 @@
+food = %W(bacon orange apple)
+p food(0)
+p food(-1)
+food << "yogurt"
+p food
+countries = {
+	ES: "Spain", AT: "Austria", BE: "Belgium"
+}
+p countries
+course = "course"
+p course.gsub("ourse", 'anada').upcase
