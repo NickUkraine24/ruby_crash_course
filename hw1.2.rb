8cb704834651987ac17c346a20cfbbdd83a0729f
0xKolyanus <mukola.1zt.@gmail.com>
2021-04-17 22:37:20 +0300

    Deleted unnecessary files

--- hw1.2.rb
+++
@@ -1,9 +0,0 @@
-food = %w(bacon orange apple)
-p food[0]
-p food[food.size-1]
-food<<"yogurt"
-p food
-countries = {ES: "Spain", AT: "Austria", BE: "Belgium"}
-string = "Course"
-string = string.sub("ourse", "anada").upcase
-p string
