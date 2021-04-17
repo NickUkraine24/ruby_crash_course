51dc29e4498b5b42b910c1f04403bf2c4e9c8bf5
Sasha-Kuba <82408177+Sasha-Kuba@users.noreply.github.com>
2021-04-17 23:31:21 +0300

    Add files via upload

---
+++ hw1.2.rb
@@ -0,0 +1,16 @@
+
+food = ["bacon" , "orange" , "apple"]
+puts food.at(0)
+puts food[2]
+
+food << "yogurt"
+puts food[3]
+
+hash  = {Uk: 'Ukraine', Ro: 'Romania' ,PO: 'Poland'}
+puts hash
+
+string = 'course'
+string = string.gsub('ourse','anada')
+puts string
+
+puts string.upcase
