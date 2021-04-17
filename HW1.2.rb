7c56b8bf3d03f731dbb6e6375c400981b9d3d237
MaksMaryanchuk <64914675+maryanchuk04@users.noreply.github.com>
2021-04-17 23:52:53 +0300

    Add files via upload

---
+++ HW1.2.rb
@@ -0,0 +1,20 @@
+Eat = ["Bacon", "Orange", "Apple"]
+p Eat
+
+p Eat.first;
+p Eat.last;
+
+Eat.push("Yogurt")
+
+
+country = {
+    UK: 'United KIndom',
+    GER: 'Germany',
+    IS: 'Island'
+}
+
+p country
+
+course = "course"
+
+p course.gsub("ourse", "anada").upcase
