657090adc488c2de08dd686d05d5b318483efa5c
VladyslavGennadijovych <82404622+VladyslavGennadijovych@users.noreply.github.com>
2021-04-17 12:16:47 +0300

    Add files via upload

---
+++ HW1.2.rb
@@ -0,0 +1,17 @@
+#Array
+food = ["bacon", "orange","apple"]
+p food
+p food.first
+p food.last
+food << "yogurt"
+p food
+country = {"UA"=>"Ukraine","UK"=>"United Kingdom","FR"=>"France"}
+p country
+p country["UA"]
+p country["UK"]
+p country["FR"]
+string = "course"
+#p string
+p string.upcase
+s = string.gsub("ourse","anada")
+p s.upcase
