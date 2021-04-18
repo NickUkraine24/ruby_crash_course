7465d2daef96d0f7760fab70594ecfa72f6852c7
Александра <oleksandratisenko59@gmail.com>
2021-04-18 12:51:00 +0300

    added first commit for hw1.2.rb

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,12 +1,18 @@
+#ex 1,2
 food=["bacon","orange","apple"]
 puts "the array before changes:"
 p food
+#ex 3
 puts "the first element is #{food[0]}"
+#ex4
 puts "the last element is #{food[-1]}"
+#ex5
 puts "array after changes:"
 p food<<"yogurt"
-countries = {DE: "Germany", IT: 'Italy', UA:"Ukraine"}
+#ex 6
+countries={DE: "Germany", IT: 'Italy', UA:"Ukraine"}
 puts countries
+#ex7
 p"string before changes:"
 p "course"
 p"string after changes:"
