8f558fc8f77c7d55912eac8fa20cccee3543c626
0xKolyanus <mukola.1zt.@gmail.com>
2021-04-18 08:11:59 +0300

    add comment in hw1.2

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,10 +1,21 @@
+# ex2
 food = %W(bacon orange apple)
+
+# ex3
 p food.dig(0)
+
+# ex4
 p food[-1]
+
+# ex5
 food.push("yogurt")
 p food
+
+# ex6
 hash_1 = Hash[UA: "Ukraine", JP: "Japan", WK: "Wakanda"]
 puts hash_1
+
+# ex7
 course = "course"
 p course.gsub!("ourse", "anada")
 p course.upcase
