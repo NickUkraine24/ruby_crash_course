461a8d3c5b0f9b129c0052a61af5a1dff7dae4f5
Sasha-Kuba <sasakuba54@gmail.com>
2021-04-18 13:11:39 +0300

    added comments for hw1.2

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,16 +1,15 @@
-
+#ex1
 food = ["bacon" , "orange" , "apple"]
 puts food.at(0)
 puts food[2]
 
 food << "yogurt"
-puts food[3]
-
+puts food.last
+#ex2
 hash  = {Uk: 'Ukraine', Ro: 'Romania' ,PO: 'Poland'}
 puts hash
-
+#ex3
 string = 'course'
 string = string.gsub('ourse','anada')
 puts string
-
 puts string.upcase
