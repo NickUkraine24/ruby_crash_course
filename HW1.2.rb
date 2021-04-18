0fc1527fcb8041c09ba7795f5e1c7fd4c98d1d6d
RavliukSerhii <ravliuk.serhii@chnu.edu.ua>
2021-04-18 12:32:04 +0300

    add comments

--- HW1.2.rb
+++ HW1.2.rb
@@ -1,12 +1,24 @@
+#2
+#Create an array with the name of food with these values: “bacon”, “orange”, “apple”
+#Get the first element of the food array
+#Get the last element of the food array
+
 food = ["bacon" , "orange" , "apple"]
 puts food.first
 puts food.last
 
+#Add a new element into the array: yogurt
+
 food << "yogurt"
 puts food[3]
 
+#Create a hash with 3 key/value pairs representing country codes (like ES) & their country names (like Spain)
+
 hash  = {Uk: 'Ukraine', ES: 'Spain' ,RU: 'Russia'}
 puts hash
+
+#Create a string with name course
+#Change ourse on anada
 
 string = 'course'
 string = string.gsub('ourse','anada')
