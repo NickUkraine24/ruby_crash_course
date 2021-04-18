591173a060b8470da92b82049578c787b9d604f5
koval-oss <you@example.com>
2021-04-18 09:51:10 +0300

    .

--- hw1.2.rb
+++ hw1.2.rb
@@ -1,9 +1,15 @@
+#ex2
 food = %w(bacon orange apple)
+#ex3
 p food[0]
+#ex4
 p food[food.size-1]
+#ex5
 food<<"yogurt"
 p food
+#ex 6
 countries = {ES: "Spain", AT: "Austria", BE: "Belgium"}
+#ex 7
 string = "Course"
 string = string.sub("ourse", "anada").upcase
 p string
