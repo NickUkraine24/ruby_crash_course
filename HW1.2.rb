9f7a5c3bf147b01257bb09791817b08cd386e74c
VladyslavGennadijovych <82404622+VladyslavGennadijovych@users.noreply.github.com>
2021-04-18 10:22:58 +0300

    Update HW1.2.rb

--- HW1.2.rb
+++ HW1.2.rb
@@ -1,17 +1,22 @@
-#Array
-food = ["bacon", "orange","apple"]
+#EX1
+food = ["bacon", "orange","apple"] #Create an array with the name of food with these values: “bacon”, “orange”, “apple”.
 p food
-p food.first
-p food.last
-food << "yogurt"
+#EX2 
+p food.first #Get the first element of the food array
+#EX3
+p food.last #Get the last element of the food array.
+#Ex4
+food << "yogurt" #Add a new element into the array: yogurt.
 p food
-country = {"UA"=>"Ukraine","UK"=>"United Kingdom","FR"=>"France"}
+#EX5
+country = {"UA"=>"Ukraine","UK"=>"United Kingdom","FR"=>"France"} #reate a hash with 3 key/value pairs representing country codes
 p country
 p country["UA"]
 p country["UK"]
 p country["FR"]
-string = "course"
-#p string
+#EX6
+string = "course" #Create a string with name course
+p string
 p string.upcase
-s = string.gsub("ourse","anada")
-p s.upcase
+s = string.gsub("ourse","anada") #Change ourse on anada
+p s.upcase #uppercase string
