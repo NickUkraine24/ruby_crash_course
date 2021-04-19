e212c126204fc542fc4b92f086b03c65eed98eda
MaksMaryanchuk <lion20914king@gmail.com>
2021-04-19 20:09:50 +0300

    Done

--- HW1.2.rb
+++ HW1.2.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ex1
 Eat = ["Bacon", "Orange", "Apple"]
 p Eat
@@ -19,25 +18,4 @@ p country
 #ex3
 course = "course"
 
-=======
-Eat = ["Bacon", "Orange", "Apple"]
-p Eat
-
-p Eat.first;
-p Eat.last;
-
-Eat.push("Yogurt")
-
-
-country = {
-    UK: 'United KIndom',
-    GER: 'Germany',
-    IS: 'Island'
-}
-
-p country
-
-course = "course"
-
->>>>>>> 7c56b8bf3d03f731dbb6e6375c400981b9d3d237
 p course.gsub("ourse", "anada").upcase
