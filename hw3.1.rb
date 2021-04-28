0392bff7763d1303cc87a6bcdbd1c94ff863069f
Sasha-Kuba <82408177+Sasha-Kuba@users.noreply.github.com>
2021-04-29 02:40:56 +0300

    added  commit for hw3.1

---
+++ hw3.1.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+#ex1
+file = File.open("test_file_hw_3.1.txt", 'r')
+p file.read
+file.close
+#or
+p File.read("test_file_hw_3.1.txt")
+
+#ex2
+time = Time.now
+p time.strftime('%Y.%m.%d %H:%M:%S')
+
+#ex 3 
+person = { name: 'Den', age: 23, position1: 'ment1or' }
+
+begin 
+raise
+ peron.keys?(:position) 
+ person.values?('mentor')
+
+
+rescue Exception
+ p "This person is not developer"
+else
+p 'good'
+end
+
+#ex 4
+begin 
+ person.mmm
+rescue StandardError => e
+p "This is StandardError" 
+p e.message
+ 
+end
