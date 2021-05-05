26640f9f842e551291ee9c0da9582150a56fa7bc
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-05 13:31:24 +0300

    Added hw3.2

---
+++ hw3.2.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# Ex 1
+def greeting(name)
+  "Hello, my name is #{name}"
+end
+
+puts greeting('Artem')
+
+# Ex 2
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts "The multiplication of 2 and 3 is #{multiply(2, 3)}"
+
+# Ex 3
+def square(num)
+  num ** 2
+end
+
+puts "The square of 2 is #{square(2)}"
+
+# Ex 4
+def info_with_args(*params)
+  name = params[0]
+  birthday = params[1]
+  height = params[2]
+  weight = params[3]
+  puts "name is #{name}, birthday is #{birthday}, height is #{height}, weight is #{weight}"
+end
+
+info_with_args("Artem", 2002, 176, 92.3)
+
+# Ex 5
+def info_with_kwargs(**obj)
+  puts "name is #{obj[:name]}, birthday is #{obj[:birthday]}, height is #{obj[:height]}, weight is #{obj[:weight]}"
+end
+
+info_with_kwargs({name: "Vita", birthday: 2008, height: 160, weight: 54.2})
