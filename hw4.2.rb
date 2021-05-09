f34aaf425b091b5ae778bd3b96bb9c759d128f9b
Anastasiia Masiian <masiian.anastasiia@chnu.edu.ua>
2021-05-09 17:45:00 +0300

    hw4.2.rb done

---
+++ hw4.2.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+
+# ex1
+class Cat
+  def meow
+    puts "I'm a cat, gimme food & pet me now!"
+  end
+end
+
+cat = Cat.new
+3.times {cat.meow}
+
+# ex2
+class Rectangle
+  attr_accessor :height, :width
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  # ex3
+  def height=(value)
+    if value.negative?
+      begin
+        raise "Height should be a positive value!"
+      rescue Exception => e
+        p e.message
+      end
+    else
+      @height = value
+    end
+  end
+
+  def width=(value)
+    if value.negative?
+      begin
+      raise "Width should be a positive value!"
+      rescue Exception => e
+        p e.message
+      end
+    else
+      @width = value
+    end
+  end
+
+  def area
+    begin
+      @width * @height
+    rescue NoMethodError => e
+      p e.message
+    else
+      @width * @height
+    end
+  end
+
+end
+
+Rectangle.new(1, 4).height=(-2)
