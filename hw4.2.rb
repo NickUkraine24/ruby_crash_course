acc41ada32a557fc872284e8dcd4e7af2dca0ad8
khannah13 <hannakhomko@gmail.com>
2021-05-12 09:57:01 +0300

    added first commit for hw4.2

--- hw4.2.rb
+++ hw4.2.rb
@@ -2,29 +2,29 @@
 
 # ex1
 class Cat
-	def meow
-		"I'm a cat, gimme food & pet me now!"
-	end	
+  def meow
+    "I'm a cat, gimme food & pet me now!"
+  end 
 end
 
 3.times do 
-	cat = Cat.new.meow 
-	p cat
+  cat = Cat.new.meow 
+  p cat
 end
 
 # ex2
 class Rectangle
-	attr_accessor :width
-	attr_accessor :height
+  attr_accessor :width
+  attr_accessor :height
 
-	def initialize (width, height)
-		@width = width
-		@height = height
-	end
+  def initialize (width, height)
+    @width = width
+    @height = height
+  end
 
-	def area 
-		@width * @height
-	end
+  def area 
+    @width * @height
+  end
 end
 
 area = Rectangle.new(20, 30).area
@@ -32,20 +32,20 @@ p "The area of the rectange is #{area}"
 
 # ex3
 class Rectangle
-	attr_accessor :width
-	attr_accessor :height
+  attr_accessor :width
+  attr_accessor :height
 
-	def initialize (width, height)
-		@width = width
-		@height = height
-		if @width < 0 || @height < 0 
-			raise "Error. Check the correctness of the entered data"
-		end
-	end
+  def initialize (width, height)
+    @width = width
+    @height = height
+    if @width < 0 || @height < 0 
+      raise "Error. Check the correctness of the entered data"
+    end
+  end
 
-	def area 
-		@width * @height
-	end
+  def area 
+    @width * @height
+  end
 end
 
 area = Rectangle.new(-70, 30).area
